7f67f6f2951509a5703893704f97f6a13f2126d3
João Torres <116197527+joaogkt@users.noreply.github.com>
2022-11-24 11:07:32 -0300

    Add files via upload

---
+++ Uteis/Cadastro/__init__.py
@@ -0,0 +1,80 @@
+from typing import List, Tuple
+def linha(tam=42):
+    return '-'*tam
+
+
+def leiaint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO! Digite um numero inteiro válido.\033[m')
+            continue
+        else:
+            return n
+
+
+def cabeçalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+
+def menu(lista):
+    cabeçalho('MENU PRINCIPAL')
+    c = 1
+    for i in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{i}\033[m')
+        c += 1
+    print(linha())
+    opc = leiaint('\033[32mSua opcao: \033[m')
+    return opc
+
+
+def arqExiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Houve um ERRO na criação do arquivo!')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!!')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('ERRO ao ler o arquivo')
+    else:
+        cabeçalho('PESSOAS CADASTRADAS')
+        for l in a:
+            dado = l.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Houve um erro na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Houve um ERRO na hora de escrever os dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado')
+            a.close()
