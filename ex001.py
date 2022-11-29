c0d43bc8785d221558d09024f75245d5a6ae9ab9
João Torres <116197527+joaogkt@users.noreply.github.com>
2022-11-29 14:46:36 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-ano = int(input("Digite o ano em que voce nascceu: "))
+ano = int(input("Digite o ano em que voce nasceu: "))
 agora = date.today()
 idade = agora.year - ano
 print("{} anos".format(idade))
